d020e1cd3467a5033eb774fb428932d1c5d534f7
n4ybn <david.dupre@gmail.com>
2015-07-26 15:29:21 -0400

    version1 of assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-        y <-x
+makeCacheMatrix <- function(mi = matrix()) {  ##Matrix in
+        mo <-NULL  ## Matrix Out
      
-        mx <- function() {
-                library(MASS)
-                rc <- apply(y,c(1,2),ginv)
+        set <- function(y=matrix()){
+                mi <<- y  ## Matrix incoming
+                mo  <<- NULL
         }
+        
+        get <- function() mi
+        
+        setmatrix <- function(y=matrix()) {
+                ##library(MASS)
+                mo <<- y
+        }
+        
+        getmatrix <- function() mo
+        
+        list (set=set, get=get, setmatrix=setmatrix,getmatrix=getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(mi=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+        mo <- mi$getmatrix()
+        if (!is.null(mo)) {
+                message("getting cached matrix")
+                return(mo)
+        }
+        data <- mi$get()
+        rc <- apply(y,c(1,2),solve)
+        x$setmean(mo)
+        mo
 }
