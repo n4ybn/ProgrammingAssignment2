5fb91eee24342612a53876e80e3ff7db6cd67809
n4ybn <david.dupre@gmail.com>
2015-07-26 17:04:02 -0400

    Completed version 2 of the Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
-## DLD: This program creates a Matrix with four functions.
+## DLD: July 26, 2015 
+## This program creates a Matrix with four functions.
 ## GET,SET,GETMATRIX, SETMATRIX
 ## These function similarly to the Vector example in the assignment.
-## 
+## 1. Set up an input Matrix like this:
+## 2.   a <- matrix(31:66,6,6)
+## 3. Execute function like this: source(D:\your_folder\cachematrix.R)
+## 4. mtx <- cacheSolve(a)
+## 5. mtx$get() - returns the input matrix
+## 6. mtx$getmatrix() - returns the inverse of the matrix
+
 
 ## This Function: makeCacheMatrix 
 ## accepts a matrix and returns a matrix with four sub functions
